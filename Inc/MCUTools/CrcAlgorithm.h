4a2ac3f98b4610744480b5c4e3cbdba257a219f3
Виталий Крюков <Nomarus@outlook.com>
2026-08-21 13:55:04 +0500

    typedef for crc8_func

--- Inc/MCUTools/CrcAlgorithm.h
+++ Inc/MCUTools/CrcAlgorithm.h
@@ -7,16 +7,20 @@ extern "C" {
 #endif
 
 
-
+typedef uint8_t (*crc8_Func)(uint8_t* data, size_t size);
 
 /// @brief Calculate CRC for present data
 /// @param data 
 /// @param size 
 /// @param crcStartValue 
 /// @param polynom 
-/// @return 
+/// @return
 uint8_t crc8_calculate(uint8_t* data, size_t size, uint8_t crcStartValue, uint8_t polynom);
 
+/// @brief Calculate crc with polynom 0x31 and start value 0x00
+/// @param data 
+/// @param size 
+/// @return 
 uint8_t crc8_DallasMaxim(uint8_t* data, size_t size);
 
 
